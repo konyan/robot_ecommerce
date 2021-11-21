44c66320865ca8ef82f862eb0e9a43e00052f4bf
Nyan Lin Tun <nyanlin@grow.ai>
2021-11-21 23:47:37 +0630

    setup redux and cart reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <PersistGate persistor={persistor} loading={null}>
+          <App />
+        </PersistGate>
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
